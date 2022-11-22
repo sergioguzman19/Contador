d4967e886224d00a8238dc86eaad0a430afd2110
sergioguzman <sergio.guzman1905@gmail.com>
2022-11-22 14:57:20 -0500

    Editing the HTML

--- home.js
+++ home.js
@@ -1,12 +1,12 @@
-// select elements from the html.
+// Select elements from the html.
 weeks =  document.getElementById('semanas');
 days =  document.getElementById('dias');
 
-// obtain the dates and time stamps. 
+// Obtain the dates and time stamps. 
 let future = new Date('2023-06-01');
 let actual = new Date();
 
-// calculate de difference.
+// Calculate de difference.
 
 let dayInMs = 86400000;
 
@@ -17,7 +17,7 @@ let difDays = Math.round(diff / dayInMs);
 let difWeeks = Math.round(difDays / 7) ;
 
 
-// populate the html.
+// Populate the html.
 
 days.innerHTML = difDays;
 weeks.innerHTML = difWeeks;
