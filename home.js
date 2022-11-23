63eb01ab255af519c31199c9d57fc3546317b93c
sergioguzman <sergio.guzman1905@gmail.com>
2022-11-22 22:52:03 -0500

    adding button

--- home.js
+++ home.js
@@ -1,23 +1,37 @@
 // Select elements from the html.
 weeks =  document.getElementById('semanas');
 days =  document.getElementById('dias');
+load = document.getElementById("load");
+head = document.getElementById('head');
+companing1 = document.getElementById('companing1');
+companing2 = document.getElementById('companing2');
+companingD1 = document.getElementById('companing-dias1');
+companingD2 = document.getElementById('companing-dias2');
+
+load.addEventListener('click', calculate);
 
 // Obtain the dates and time stamps. 
 let future = new Date('2023-06-01');
 let actual = new Date();
 
-// Calculate de difference.
+function calculate() {
+    // Calculate de difference.
 
-let dayInMs = 86400000;
+    let dayInMs = 86400000;
 
-let diff = future - actual;
+    let diff = future - actual;
 
-let difDays = Math.round(diff / dayInMs);
+    let difDays = Math.round(diff / dayInMs);
 
-let difWeeks = Math.round(difDays / 7) ;
+    let difWeeks = Math.round(difDays / 7) ;
 
-
-// Populate the html.
-
-days.innerHTML = difDays;
-weeks.innerHTML = difWeeks;
+    // Populate the html.
+    load.style.visibility="hidden";
+    head.innerHTML = 'Para el dia F:';
+    companing1.style.visibility='visible';
+    companing2.style.visibility='visible';
+    weeks.innerHTML = difWeeks;
+    companingD1.style.visibility='visible';
+    companingD2.style.visibility='visible';
+    days.innerHTML = difDays;
+};
