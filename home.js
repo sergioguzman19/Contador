66ac459a5b0f949d4d794a7c5c9f001b1e147ca4
sergioguzman <sergio.guzman1905@gmail.com>
2022-11-23 09:50:54 -0500

    adding divs

--- home.js
+++ home.js
@@ -3,10 +3,7 @@ weeks =  document.getElementById('semanas');
 days =  document.getElementById('dias');
 load = document.getElementById("load");
 head = document.getElementById('head');
-companing1 = document.getElementById('companing1');
-companing2 = document.getElementById('companing2');
-companingD1 = document.getElementById('companing-dias1');
-companingD2 = document.getElementById('companing-dias2');
+div = document.getElementById('mainBlock');
 
 load.addEventListener('click', calculate);
 
@@ -28,10 +25,7 @@ function calculate() {
     // Populate the html.
     load.style.visibility="hidden";
     head.innerHTML = 'Para el dia F:';
-    companing1.style.visibility='visible';
-    companing2.style.visibility='visible';
+    div.style.visibility='visible';
     weeks.innerHTML = difWeeks;
-    companingD1.style.visibility='visible';
-    companingD2.style.visibility='visible';
     days.innerHTML = difDays;
 };
